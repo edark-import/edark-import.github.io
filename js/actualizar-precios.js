e0b13a38df7892ac5e2c05561745e54c27aeda66
Ervinsalva97 <melvin23453@gmail.com>
2025-06-19 16:21:11 -0500

    Add GitHub Actions workflow for updating prices and refactor Firebase config to use environment variables

--- js/actualizar-precios.js
+++ js/actualizar-precios.js
@@ -3,15 +3,15 @@ const firebase = require('firebase/compat/app');
 require('firebase/compat/firestore');
 const axios = require('axios');
 
-// Configuración de Firebase (usa tus credenciales reales)
+// Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-    apiKey: "AIzaSyBXbqr_NrwqcfDvvk1mqN9GKKPaRv4uvx4",
-    authDomain: "edark-proyect.firebaseapp.com",
-    projectId: "edark-proyect",
-    storageBucket: "edark-proyect.appspot.com",
-    messagingSenderId: "705929141287",
-    appId: "1:705929141287:web:f16389625de554d6790202",
-    measurementId: "G-V89W8SCSH1"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
