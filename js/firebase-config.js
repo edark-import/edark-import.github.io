a47ec4d29155b862df9076046c52b60456977f1f
Richard Gutierrez <rcgvaliente@gmail.com>
2025-11-28 13:21:41 -0500

    Firebase: expose window.firebaseConfig for admin pages

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,5 @@
 // Configuración de Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBXbqr_NrwqcfDvvk1mqN9GKKPaRv4uvx4",
   authDomain: "edark-proyect.firebaseapp.com",
@@ -8,6 +9,12 @@ const firebaseConfig = {
   appId: "1:705929141287:web:f16389625de554d6790202",
   measurementId: "G-V89W8SCSH1"
 };
+
+// Expose globally for non-module pages (admin/login.html loads this file before ES module SDK)
+window.firebaseConfig = firebaseConfig;
+
+// Optional: UMD-style export for bundlers
+try { module && (module.exports = { firebaseConfig }); } catch {}
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
